31326904aa788a4bef11533d4e30e6df272c968a
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-13 17:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", done);
+	function done() {
+    const message = "DOM load success";
+    document.body.innerHTML = message;
+  };
